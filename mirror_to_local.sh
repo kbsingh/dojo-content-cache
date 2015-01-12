9b7a530139db32e540b1fdec8df3a63375854ea2
Karanbir Singh <kbsingh@karan.org>
2015-01-12 17:43:32 +0000

    setup target for cirros image

--- mirror_to_local.sh
+++ mirror_to_local.sh
@@ -11,5 +11,4 @@
 # RDO Content
 # http://buildlogs.centos.org/centos/7/cloud/openstack-rdo/
 # also need cirros images ( check packstact to see what url it needs to end up at ) http://download.cirros-cloud.net/ 
-
-
+# http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img is where it needs to end up, look at https://github.com/stackforge/packstack/blob/master/packstack/plugins/provision_700.py#L21 
