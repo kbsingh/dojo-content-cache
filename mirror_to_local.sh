13cd92265e2b8460b91e1f962bb72678836a339b
Karanbir Singh <kbsingh@karan.org>
2015-01-12 17:35:46 +0000

    rdo content for tutorial

--- mirror_to_local.sh
+++ mirror_to_local.sh
@@ -7,3 +7,9 @@
 # CentOS-7 Atomic Images
 
 # CentOS-7 Atomic repo
+
+# RDO Content
+# http://buildlogs.centos.org/centos/7/cloud/openstack-rdo/
+# also need cirros images ( check packstact to see what url it needs to end up at )
+
+
